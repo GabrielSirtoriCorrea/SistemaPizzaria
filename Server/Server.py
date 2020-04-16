21b2fd3d096002f092f00ac9c78a990c0ae349c9
Gazebo <gabrielsirtori.correa@gmail.com>
2020-04-15 21:52:02 -0300

    ServerFinished

---
+++ Server/Server.py
@@ -0,0 +1,52 @@
+import socketserver
+from datetime import datetime
+import json, time, os, DataBaseConnection
+
+
+'''jsonTest = {
+        'ID': 'Testando',
+        'client': 'banco',
+        'phone': 'de',
+        'address': 'dados',
+        'pizzaType': 'no',
+        'pizzaBorder': 'novo',
+        'pizzaQntdTastes': 1,
+        'pizzaTaste1': 'sistema',
+        'pizzaTaste2': 'do',
+        'comments': 'gazebo',
+        'totalPrice': 1,
+        'status': 'boladao'}'''
+
+#myHost = '192.168.0.113'
+myHost = '192.168.0.5'
+myPort = 5000
+
+class ClientManage(socketserver.BaseRequestHandler):
+    def handle(self):
+
+        hora = datetime.now()
+        print(f'Conectado por: {self.client_address} as {hora.hour}:{hora.minute}')
+
+        data = self.request.recv(1024).decode('utf-8')
+        print(data)
+
+        data = json.loads(data)
+
+        if data['ID'] == 'AppSendNewRequest':
+            DataBase = DataBaseConnection.DataBaseConnection()
+            DataBase.InsertNewRequest(json.dumps(data))
+            print(DataBase.getAllRequests())
+                
+        elif data['ID'] == 'DesktopFinishRequest':
+            DataBase = DataBaseConnection.DataBaseConnection()
+            DataBase.setFinishRequest(data['client'])
+            print(DataBase.getAllRequests())
+
+        elif data['ID'] == 'DesktopGetAllRequests':
+            DataBase = DataBaseConnection.DataBaseConnection()
+            self.request.send(json.dumps(DataBase.getAllRequests()).encode())
+
+
+adress = (myHost, myPort)
+server = socketserver.ThreadingTCPServer(adress, ClientManage)
+server.serve_forever()
